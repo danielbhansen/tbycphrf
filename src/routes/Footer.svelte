1840f67efc161719188b19fac8263ffac9267ee2
danielbhansen <dh@eleven-seventeen.com>
2025-07-15 14:01:07 -0400

    version number update

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,4 +1,4 @@
 <footer>
     <p class="qualification"><small>NOTE: All calculations are for estimation purposes only, and are not official race results.</small></p>
-    <p class="qualification"><small>v3.0</small></p>
+    <p class="qualification"><small>v3.0.1</small></p>
 </footer>
